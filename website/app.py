ee49e8632157b9ec2e7374589761faffe939c1b9
callegarostefano <callegarostefano@gmail.com>
2025-11-17 16:36:48 +0100

    404 css js

--- website/app.py
+++ website/app.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template
 from website.routes.predict import predict_bp
 
-# App Flask
 app = Flask(
     __name__,
-    static_url_path='/als-differential-analysis/static',  # URL pubblico statici
-    static_folder='static'                                # cartella fisica statici, relativa a app.py
+    static_url_path='/als-differential-analysis/static',  # URL pubblico
+    static_folder='static'                                # cartella fisica relativa a app.py
 )
 
-# Registrazione blueprint predict
+# registra blueprint
 app.register_blueprint(predict_bp, url_prefix='/als-differential-analysis')
 
-# Route principale
+# route principale
 @app.route('/als-differential-analysis/')
 def index():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
